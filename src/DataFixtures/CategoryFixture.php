4c081db0a094b120c7b46bf44814b9fcadcefe43
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2023-11-22 11:38:54 +0100

    feat(fixture) correct for psr2

--- src/DataFixtures/CategoryFixture.php
+++ src/DataFixtures/CategoryFixture.php
@@ -5,18 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class CategoryFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i=1; $i < 11; $i++) {
+        for ($i = 1; $i < 11; $i++) {
             $category = new Category();
-            $category ->setName('catergory'. $i);
+            $category ->setName('catergory' . $i);
             $manager->persist($category);
 
-            $this->addReference('category_' .$i, $category);
+            $this->addReference('category_' . $i, $category);
             // peremet que la catégorie soit dispo dans les autres fixture
         }
 
