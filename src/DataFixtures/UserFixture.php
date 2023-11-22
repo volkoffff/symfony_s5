2e2364c135febad4aef9262e2118e2a35b134df0
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2023-11-22 11:44:04 +0100

    feat(all) unversioned files

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixture extends Fixture
+{
+    public function __construct(protected UserPasswordHasherInterface $passwordHasherInterface)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (range(1, 5) as $i) {
+            $user = new User();
+            $user -> setEmail('email' . $i . '@gmail.com');
+            $user -> setPassword($this->passwordHasherInterface->hashPassword(
+                $user,
+                'password'
+            ));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
